1cf266e0800fdd73cd448af178135f0119913afd
Alla Kuzminova <alchik@gmail.com>
2020-09-01 20:12:34 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,33 @@
 # Helper Method
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,4,8],
+  [2,4,6],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8]
+]
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |win_combination|
+    (board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X") ||
+    (board[win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O")
+ end
+end
+
+def full?(board) 
+  board.select{ |item| item != " " && !item.nil? && item != ""}.size == 9
+end
+
+value = won?([" "," "," "," "," "," "," "," "," "])
+value1 = full?([" "," "," "," "," "," "," "," "," "])
+
+puts "#{value} #{value1}"
+
 
 # Define your WIN_COMBINATIONS constant
