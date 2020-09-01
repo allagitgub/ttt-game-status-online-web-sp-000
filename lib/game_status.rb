b22df69b46dfd0bc92f37ac4ef2e1a4326af472f
Alla Kuzminova <alchik@gmail.com>
2020-09-01 21:06:28 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -38,7 +38,7 @@ def winner(board)
       (board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X") }
     val = arr.size > 0 ? "X" : "O"
     puts "size #{arr.size} and #{val}"
-    WIN_COMBINATIONS.detect {|win_combination|
+    return WIN_COMBINATIONS.detect {|win_combination|
       (board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X") }.size > 0 ? "X" : "O"
   end
    nil
